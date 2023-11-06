aa5c59d887b68fa286205240aa51347ac6a935d0
Piotr Kaleta <118031180+Facio122@users.noreply.github.com>
2023-11-06 21:47:10 +0100

    day 3 push

--- app/src/main/java/com/example/exchangeapp/Calculator.kt
+++ app/src/main/java/com/example/exchangeapp/Calculator.kt
@@ -1,5 +1,6 @@
 package com.example.exchangeapp
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -10,16 +11,22 @@ import android.view.MenuItem
 import android.view.View.OnCreateContextMenuListener
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.SearchView
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.io.Serializable
 
 @Suppress("DEPRECATION")
 class Calculator : AppCompatActivity() {
 
-    var dataList = mutableListOf<ExchangeRate>()
+    private var dataList = mutableListOf<ExchangeRate>()
+    private var fromCurrency = "USD"
+    private var toCurrency = "EUR"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +38,7 @@ class Calculator : AppCompatActivity() {
 
         val imageBtnFromCurr = findViewById<ImageButton>(R.id.setFromCurrencyBtn)
         val imageBtnToCurr = findViewById<ImageButton>(R.id.setToCurrencyBtn)
+        val convertBtn = findViewById<Button>(R.id.convertBtn)
 
         val receivedBundle = intent.extras
         if(receivedBundle != null)
@@ -39,13 +47,38 @@ class Calculator : AppCompatActivity() {
         }
 
         imageBtnFromCurr.setOnClickListener{
-            showSearchWindow()
+            showSearchWindow(1)
         }
 
         imageBtnToCurr.setOnClickListener{
-            showSearchWindow()
+            showSearchWindow(2)
         }
 
+        convertBtn.setOnClickListener{
+            onClick()
+        }
+
+    }
+
+    private fun onClick()
+    {
+        val editText = findViewById<EditText>(R.id.editTextNumberSigned).text.toString().trim()
+        var quantityDouble: Double = if(editText.isEmpty())
+            0.0
+        else {
+            editText.toDouble()
+        }
+
+        val fromCash = dataList.find {
+            it.code == fromCurrency
+        }
+        val toCash = dataList.find {
+            it.code == toCurrency
+        }
+
+        val result = quantityDouble * (fromCash!!.mid / toCash!!.mid)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        resultTextView.text = String.format("%.2f", result) + " " + toCash.code
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -57,6 +90,13 @@ class Calculator : AppCompatActivity() {
         var itemView = item.itemId
         when(itemView){
             R.id.switchCalculator ->{
+                var temp = fromCurrency
+                fromCurrency = toCurrency
+                toCurrency = temp
+                findViewById<TextView>(R.id.fromCurrencyTextView).text = fromCurrency
+                findViewById<TextView>(R.id.toCurrencyTextViewTop).text = toCurrency
+                findViewById<TextView>(R.id.toCurrencyTextView).text = fromCurrency
+                onClick()
             }
             R.id.back ->{
                 finish()
@@ -65,7 +105,7 @@ class Calculator : AppCompatActivity() {
         return false
     }
 
-    private fun showSearchWindow()
+    private fun showSearchWindow(option: Int?)
     {
         val dialog = Dialog(this)
         val window = dialog.window
@@ -75,6 +115,31 @@ class Calculator : AppCompatActivity() {
         window?.setGravity(Gravity.TOP)
         dialog.show()
         val searchView = dialog.findViewById<SearchView>(R.id.searchRecyclerView)
+
+        var recyclerView = dialog.findViewById<RecyclerView>(R.id.recyclerView)
+        var recyclerAdapter = RecyclerAdapter(dataList)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = recyclerAdapter
+
+        recyclerAdapter.onItemClick = {
+            val fromCurrencyText = findViewById<TextView>(R.id.fromCurrencyTextView)
+            val toCurrencyText = findViewById<TextView>(R.id.toCurrencyTextView)
+            val toCurrencyTextTop = findViewById<TextView>(R.id.toCurrencyTextViewTop)
+            if(option == 1)
+            {
+                fromCurrency = it.code
+                fromCurrencyText.text = it.code
+                toCurrencyText.text = it.code
+            }
+            else if(option == 2)
+            {
+                toCurrency = it.code
+                toCurrencyTextTop.text = it.code
+            }
+            dialog.dismiss()
+        }
+
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener
         {
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -82,12 +147,13 @@ class Calculator : AppCompatActivity() {
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                filteredList(newText)
+                if (newText != null) {
+                    recyclerAdapter.setFilterItemList(filteredList(newText))
+                    recyclerView.adapter = recyclerAdapter
+                }
                 return true
             }
         })
-        val recyclerView = dialog.findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.setHasFixedSize(true)
     }
 
     private fun filteredList(query: String?) : List<ExchangeRate>
--- app/src/main/java/com/example/exchangeapp/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/exchangeapp/RecyclerViewAdapter.kt
@@ -1,49 +1,52 @@
 package com.example.exchangeapp
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdapter(private val dataSet: Array<String>) :
-    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+class RecyclerAdapter(private var list: List<ExchangeRate>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>()
+{
+    var onItemClick : ((ExchangeRate) -> Unit)? = null
+    var flag = true
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder)
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        init {
-            // Define click listener for the ViewHolder's View
-            val codeTextView = view.findViewById<TextView>(R.id.codeRecyclerView)
-            val nameTextView = view.findViewById<TextView>(R.id.nameRecyclerView)
-
-            codeTextView.setOnClickListener{
-
-            }
-        }
+    fun setFilterItemList(filteredList: List<ExchangeRate>)
+    {
+        this.list = filteredList
     }
 
-    // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.recyclerviewitem, viewGroup, false)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        val codeTextView: TextView = itemView.findViewById(R.id.codeRecyclerView)
+        val nameTextView: TextView = itemView.findViewById(R.id.nameRecyclerView)
+    }
 
+    @SuppressLint("ResourceAsColor")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recyclerviewitem, parent, false)
+        flag = if(flag) {
+            false
+        } else {
+            view.setBackgroundColor(androidx.appcompat.R.color.primary_material_light)
+            true
+        }
         return ViewHolder(view)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
-        viewHolder.textView.text = dataSet[position]
+    override fun getItemCount(): Int {
+        return list.size
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = dataSet.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = list[position]
+        holder.codeTextView.text = item.code
+        holder.nameTextView.text = item.currency
 
+        holder.itemView.setOnClickListener{
+            onItemClick?.invoke(item)
+        }
+    }
 }
